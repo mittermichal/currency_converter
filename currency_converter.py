428d057eaae5e8afe6bf6f0f4547eb373f4c1da7
Michal Mitter <mittermichal@gmail.com>
2017-07-04 11:23:21 +0200

    priority of USD and GBP symbol

--- currency_converter.py
+++ currency_converter.py
@@ -16,10 +16,15 @@ parser.add_argument('--output_currency', metavar='<3 letter currency code>',
 #https://github.com/MicroPyramid/forex-python/blob/80a14321ea8ecc74fc277dcd3770c685d6442cb3/forex_python/converter.py#L115
 #https://github.com/MicroPyramid/forex-python/blob/9b479b76bdf05c210236550bbee47970c9e54a8c/forex_python/raw_data/currencies.json
 def currency_symbol_to_code(symbol):
+    if symbol=='$':
+        return 'USD'
+    elif symbol=='£':
+        return 'GBP'
     with open('currencies.json') as f:
         currency_data = json.loads(f.read())
     currency_dict = next((item['cc'] for item in currency_data if item["symbol"] == symbol), None)
     return currency_dict
 
 #print(args)
-print(currency_symbol_to_code('£'))
+for symbol in '₫₹€£$¥-₪':
+    print(currency_symbol_to_code(symbol))
